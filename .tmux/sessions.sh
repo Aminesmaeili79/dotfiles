0e5d373bbd9ec24cd22c864af729a377b5a42e1c
Amin Esmaeili <aminesmaeili79@yahoo.com>
2025-06-03 13:46:55 +0300

    auto: update dotfiles

--- .tmux/sessions.sh
+++ .tmux/sessions.sh
@@ -9,50 +9,97 @@ if ! command -v tmux >/dev/null 2>&1; then
   exit 1
 fi
 
-# 1-development session
-if ! tmux has-session -t 1-dev 2>/dev/null; then
-  tmux new-session -d -s 1-dev
+if ! tmux has-session -t pane-1 2>/dev/null; then
+  tmux new-session -d -s pane-1
 
-  tmux rename-window -t 1-dev:0 'main'
-  tmux send-keys -t 1-dev:0 'cd ~/Desktop/projects/' C-m 'clear' C-m
+  tmux rename-window -t pane-1:0 'window-1'
+  tmux send-keys -t pane-1:0 'cd ~/' C-m 'clear' C-m
 
-  tmux new-window -t 1-dev:1 -n 'Udemy'
-  tmux send-keys -t 1-dev:1 'cd ~/Desktop/projects/Udemy/' C-m 'clear' C-m
+  tmux new-window -t pane-1:1 -n 'window-2'
+  tmux send-keys -t pane-1:1 'cd ~/' C-m 'clear' C-m
 
-  tmux new-window -t 1-dev:2 -n 'Websure'
-  tmux send-keys -t 1-dev:2 'cd ~/Desktop/projects/Websure/' C-m 'clear' C-m
+  tmux new-window -t pane-1:2 -n 'window-3'
+  tmux send-keys -t pane-1:2 'cd ~/' C-m 'clear' C-m
 
-  tmux new-window -t 1-dev:3 -n 'Personal'
-  tmux send-keys -t 1-dev:3 'cd ~/Desktop/projects/Personal/' C-m 'clear' C-m
+  tmux new-window -t pane-1:3 -n 'window-4'
+  tmux send-keys -t pane-1:3 'cd ~/' C-m 'clear' C-m
 
-  tmux select-window -t 1-dev:0
+  tmux select-window -t pane-1:0
 fi
 
-# 2-workspace session
-if ! tmux has-session -t 2-workspace 2>/dev/null; then
-  tmux new-session -d -s 2-workspace
+if ! tmux has-session -t pane-2 2>/dev/null; then
+  tmux new-session -d -s pane-2
 
-  tmux rename-window -t 2-workspace:0 'notes'
-  tmux send-keys -t 2-workspace:0 'cd ~ 2>/dev/null || mkdir -p ~/' C-m 'clear' C-m
+  tmux rename-window -t pane-2:0 'window-1'
+  tmux send-keys -t pane-2:0 'cd ~/' C-m 'clear' C-m
 
-  tmux new-window -t 2-workspace:1 -n 'workspace-1'
-  tmux send-keys -t 2-workspace:1 'cd ~' C-m 'clear' C-m
+  tmux new-window -t pane-2:1 -n 'window-2'
+  tmux send-keys -t pane-2:1 'cd ~/' C-m 'clear' C-m
 
-  tmux new-window -t 2-workspace:2 -n 'workspace-2'
-  tmux send-keys -t 2-workspace:2 'cd ~' C-m 'clear' C-m
+  tmux new-window -t pane-2:2 -n 'window-3'
+  tmux send-keys -t pane-2:2 'cd ~/' C-m 'clear' C-m
 
-  tmux select-window -t 2-workspace:0
+  tmux new-window -t pane-2:3 -n 'window-4'
+  tmux send-keys -t pane-2:3 'cd ~/' C-m 'clear' C-m
+
+  tmux select-window -t pane-2:0
 fi
 
-# 3-dotfiles session
-if ! tmux has-session -t 3-dotfiles 2>/dev/null; then
-  tmux new-session -d -s 3-dotfiles
+if ! tmux has-session -t pane-3 2>/dev/null; then
+  tmux new-session -d -s pane-3
 
-  tmux rename-window -t 3-dotfiles:0 'dotfiles'
-  tmux send-keys -t 3-dotfiles:0 'cd ~/Desktop/dotfiles 2>/dev/null || mkdir -p ~/Desktop/dotfiles' C-m 'clear' C-m
+  tmux rename-window -t pane-3:0 'window-1'
+  tmux send-keys -t pane-3:0 'cd ~/' C-m 'clear' C-m
+
+  tmux new-window -t pane-3:1 -n 'window-2'
+  tmux send-keys -t pane-3:1 'cd ~/' C-m 'clear' C-m
+
+  tmux new-window -t pane-3:2 -n 'window-3'
+  tmux send-keys -t pane-3:2 'cd ~/' C-m 'clear' C-m
+
+  tmux new-window -t pane-3:3 -n 'window-4'
+  tmux send-keys -t pane-3:3 'cd ~/' C-m 'clear' C-m
+
+  tmux select-window -t pane-3:0
+fi
+
+if ! tmux has-session -t pane-4 2>/dev/null; then
+  tmux new-session -d -s pane-4
+
+  tmux rename-window -t pane-4:0 'window-1'
+  tmux send-keys -t pane-4:0 'cd ~/' C-m 'clear' C-m
+
+  tmux new-window -t pane-4:1 -n 'window-2'
+  tmux send-keys -t pane-4:1 'cd ~/' C-m 'clear' C-m
+
+  tmux new-window -t pane-4:2 -n 'window-3'
+  tmux send-keys -t pane-4:2 'cd ~/' C-m 'clear' C-m
+
+  tmux new-window -t pane-4:3 -n 'window-4'
+  tmux send-keys -t pane-4:3 'cd ~/' C-m 'clear' C-m
+
+  tmux select-window -t pane-4:0
+fi
+
+if ! tmux has-session -t pane-5 2>/dev/null; then
+  tmux new-session -d -s pane-5
+
+  tmux rename-window -t pane-5:0 'window-1'
+  tmux send-keys -t pane-5:0 'cd ~/' C-m 'clear' C-m
+
+  tmux new-window -t pane-5:1 -n 'window-2'
+  tmux send-keys -t pane-5:1 'cd ~/' C-m 'clear' C-m
+
+  tmux new-window -t pane-5:2 -n 'window-3'
+  tmux send-keys -t pane-5:2 'cd ~/' C-m 'clear' C-m
+
+  tmux new-window -t pane-5:3 -n 'window-4'
+  tmux send-keys -t pane-5:3 'cd ~/' C-m 'clear' C-m
+
+  tmux select-window -t pane-5:0
 fi
 
 # Attach to 1-dev session by default if no session is specified
 if [ -z "$TMUX" ]; then
-  tmux attach-session -t 1-dev
+  tmux attach-session -t pane-1
 fi
--- .zshrc
+++ .zshrc
@@ -214,3 +214,5 @@ export PATH="$PATH:$HOME/.dotnet/tools/"
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+alias awsvpn='cd /opt/awsvpnclient/ && ./"AWS VPN Client"'
